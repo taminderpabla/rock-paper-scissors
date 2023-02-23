95403098d95edb427137f4a2a1d2c64f529875ad
Taminder Pabla <98865761+taminderpabla@users.noreply.github.com>
2023-02-23 00:03:32 -0500

    Increased functionality through player rating

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,21 @@
+import java.io.*;
 import java.util.Random;
 
 // Game logic
 class Game {
     static final String[] arrMoves = {"rock", "paper", "scissors"};
+    static int rating = 0;
 
     // RULEBOOK
     public void printRulebook() {
-        System.out.println("RULEBOOK: \nMoves: rock, paper, scissors \nEnd Game Instruction: !exit \n");
+        System.out.println("RULEBOOK (Instructions): " +
+                "\n---------------------------------------------------------------------");
+        System.out.println("Playable Move: rock, paper, scissors \nGet Rating: !rating \nEnd Game: !exit \n");
+    }
+
+    public void matchBanner(String userName) {
+        System.out.println("\nLET'S PLAY: " + userName + " VS COMPUTER\n" +
+                "---------------------------------------------------------------------");
     }
 
     // RANDOMLY SELECT COMPUTER MOVE
@@ -23,16 +32,33 @@ class Game {
         // draw
         if (userMove.equals(compMove)) {
             System.out.printf("Draw! (%s)%n%n", compMove);
+            rating += 5;
         }
         // user wins
         else if (((userMove.equals("rock") && compMove.equals("scissors")) ||
                 userMove.equals("paper") && compMove.equals("rock")) ||
                 (userMove.equals("scissors") && compMove.equals("paper"))) {
             System.out.printf("You Win! Computer(%s)%n%n", compMove);
+            rating += 10;
         }
         // computer wins
         else {
             System.out.printf("Sorry, Computer Wins! Computer(%s)%n%n", compMove);
         }
+    }
+
+    // RETURNS RATING
+    public static int getRating() {
+        return rating; // updated rating
+    }
+
+    // SEARCH: "ratings.txt" for specified player rating
+    public int historicalPlayerRating(String userName) {
+        return 0;  // default return
+    }
+
+    // UPDATES RATING FILE
+    public void updateRecords() {
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import java.util.*;
 
 /*
-* ROCK PAPER SCISSORS: Phase 3 (b)
-* Play multiple games.
-* OOP principles and design. Improved structure of program.
+* ROCK PAPER SCISSORS: Phase 4 (a)
+* Player rating per match
 * */
 
 // Driver program
@@ -17,15 +16,26 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Initialize username
+        System.out.println("Enter your name: ");
+        String userName = scanner.nextLine().toUpperCase().trim(); //usernames are only uppercase
+        game.matchBanner(userName); // banner
+        // Initialize player rating
+        int rating; // VARIABLE to initialize historical rating from "ratings.txt"
+
         gameplay:
         while (true) {
-            System.out.println("Please make a move or instruction: ");
+            System.out.println("Please enter instruction: ");
             String userMove = scanner.nextLine().toLowerCase().trim(); // improve input reading
             switch (userMove) {
                 case "rock":
                 case "paper":
                 case "scissors":
-                    game.playMatch(userMove);
+                    game.playMatch(userMove); // GAMEPLAY
+                    break;
+                case "!rating":
+                    rating = Game.getRating(); // get current/updated rating
+                    System.out.println("Current rating: " + rating);
                     break;
                 case "!exit":
                     break gameplay; // stop match repetition
